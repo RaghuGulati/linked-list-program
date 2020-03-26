66ba1248b894f71e719276c01d24601bfd5e7ece
RaghuGulati <gulatiraghu@gmail.com>
2020-03-26 23:54:26 +0530

    Added the size function and modified push function

--- linked_list_prg.c
+++ linked_list_prg.c
@@ -7,18 +7,28 @@ struct Node {
     double key;
     struct Node* prev, *next;
     struct Node* prev_sorted, *sort;
-/*    struct Node* prev;
-    struct Node* prev_sorted;  */
 };  
 
+//size function to calculate the size of linked list
+int size(struct Node* head) {
+   int length = 0;
+   struct Node *current;
+	
+   for(current = head; current != NULL; current = current->next){
+      length++;
+   }
+	
+   return length;
+}
+
 /*push function to add the node to the head of the list*/
-void push(struct Node** head_ref, int new_data, double new_key)  
+void push(struct Node** head_ref, struct Node** head_sort_ref, int value_t, double key_t)  
 {  
     struct Node* new_node = (struct Node*)malloc(sizeof(struct Node));  
     struct Node* current = (struct Node*)malloc(sizeof(struct Node));
  
-    new_node->data = new_data;
-    new_node->key = new_key;
+    new_node->data = value_t;
+    new_node->key = key_t;
   
     new_node->next = (*head_ref);
     new_node->prev = NULL;
@@ -26,27 +36,24 @@ void push(struct Node** head_ref, int new_data, double new_key)
     new_node->sort = NULL;
     new_node->prev_sorted = NULL;
 
-//    (*head_ref) = new_node;  
-    // if list is empty
-    if (*head_ref == NULL){
+    if (*head_sort_ref == NULL){
         *head_ref = new_node;
+	*head_sort_ref = new_node;
     }
 
-    // if the node is to be inserted at the beginning of the doubly linked list
     else {
 	    (*head_ref)->prev = new_node;
 
-	    if ((*head_ref)->key >= new_node->key) {
-	        new_node->sort = *head_ref;
+	    if ((*head_sort_ref)->key >= new_node->key) {
+	        new_node->sort = *head_sort_ref;
 	        new_node->sort->prev_sorted = new_node;
-	        *head_ref = new_node;
+	        *head_sort_ref = new_node;
 	    }
 	
 	    else {
-	        current = *head_ref;
+	        current = *head_sort_ref;
 	
-	        // locate the node after which the new node
-	        // is to be inserted
+	        // locate the node after which the new node is to be inserted
 	        while (current->sort != NULL && current->sort->key < new_node->key){
 	            current = current->sort;
 		}
@@ -62,47 +69,53 @@ void push(struct Node** head_ref, int new_data, double new_key)
 	        current->sort = new_node;
 	        new_node->prev_sorted = current;
 	    }
-   }
+    }
     (*head_ref) = new_node;
 }  
 
-
-// This function prints contents of linked list starting from the given node  
-void printList(struct Node* node)  
+//Function to print the list according to insertion order as well as key sort order
+void printList(struct Node* node, struct Node* node_sort)  
 {  
     struct Node* last;  
-//    node = main;
-
+    printf("List by insertion order: ");
     while (node != NULL) {  
         printf(" (%d, %0.2f) ", node->data, node->key);  
         last = node;  
         node = node->next; 
     }
+
+    printf("\nList by ke sort order: ");
+    while (node_sort != NULL) {  
+        printf(" (%d, %0.2f) ", node_sort->data, node_sort->key);  
+        last = node_sort;  
+        node_sort = node_sort->sort; 
+    }
+
+
 }  
   
-/* Driver program to test above functions*/
+/* Main function of the program*/
 int main()  
 {  
-    /* Start with the empty list */
+    /* initilizing head and head_sort for the linked list */
     struct Node* head = NULL;
     struct Node* head_sort = NULL;
-    //struct Node
-//    push(&head, 7, 0.62);  
-    push(&head, 1, 1.95);  
-    push(&head, 4, 1.85);
-    push(&head, 6, 1.65);
-    push(&head, 8, 1.4);
-    push(&head, 5, 0.92);
-    push(&head, 15, 0.59);
-    push(&head, 6, 0.50);
-    push(&head, 12, 1.26);
 
-//    push(&head, 7, 0.65);
-    printf("Created DLL is: ");  
-    printList(head);  
+    //struct Node
+    push(&head, &head_sort,  7, 0.62);  
+    push(&head, &head_sort,  1, 1.95);  
+    push(&head, &head_sort,  4, 1.85);
+    push(&head, &head_sort,  6, 1.65);
+    push(&head, &head_sort,  8, 1.4);
+    push(&head, &head_sort,  5, 0.92);
+    push(&head, &head_sort,  15, 0.59);
+    push(&head, &head_sort,  6, 0.50);
+    push(&head, &head_sort,  12, 1.26);
+    push(&head, &head_sort,  7, 0.65);
+    printList(head, head_sort);  
   
-  /*  int size = length(head);
-    printf("\nSize of linked list: %d \n", size);*/
+    int sz = size(head);
+    printf("\nSize of linked list: %d \n", sz);
 
     getchar();  
     return 0;  
